9faeb632d545c8f6c23279a0c5fb235d197ada81
the-it-weirdo <ddsmegh4@gmail.com>
2020-11-02 19:35:11 +0530

    Adds Tab Navigation and Icons

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 
 import MovieDetailsScreen from "../screens/MovieDetailsScreen";
 import HomeScreen from "../screens/HomeScreen";
+import AppAboutScreen from "../screens/AppAboutScreen";
 
 const AppStack = createStackNavigator();
 
-const AppNavigator = () => {
+const AppStackNavigator = () => {
   return (
     <AppStack.Navigator initialRouteName="Home">
       <AppStack.Screen name="Home" component={HomeScreen} />
@@ -16,6 +19,35 @@ const AppNavigator = () => {
         component={MovieDetailsScreen}
       />
     </AppStack.Navigator>
+  );
+};
+
+const BottomTab = createBottomTabNavigator();
+
+const AppNavigator = () => {
+  return (
+    <BottomTab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Movies") {
+            iconName = "search";
+          } else if (route.name === "About") {
+            iconName = focused ? "info" : "info-outline";
+          }
+          size += 10;
+          return <MaterialIcons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "#00aaff",
+        inactiveTintColor: "gray",
+      }}
+    >
+      <BottomTab.Screen name="Movies" component={AppStackNavigator} />
+      <BottomTab.Screen name="About" component={AppAboutScreen} />
+    </BottomTab.Navigator>
   );
 };
 
---
+++ screens/AppAboutScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const AppAboutScreen = () => {
+  return (
+    <View>
+      <Text>App About</Text>
+    </View>
+  );
+};
+
+export default AppAboutScreen;
