7b34b4bc965f0d6363753f5e85eff29c86a0e66f
the-it-weirdo <ddsmegh4@gmail.com>
2020-11-04 00:52:24 +0530

    Changes props in SearchView component: Adds more customization, provides defaultProps and modifies propTypes

--- components/Header.js
+++ components/Header.js
@@ -3,14 +3,21 @@ import { Text, View } from "react-native";
 import PropTypes from "prop-types";
 import styles from "../styles/styles";
 
-import SearchView from "../components/SearchView";
+import SearchView from "./SearchView";
 
 const Header = (props) => {
   //   const headerHeight = useHeaderHeight();
   return (
     <View style={styles.headerContainer}>
       <Text style={styles.headerTitleStyle}>{props.title}</Text>
-      <SearchView onSearchSubmit={props.onSearchSubmit} />
+      <SearchView
+        searchPrompt="Search movies"
+        searchButtonPrompt="Search"
+        containerStyle={styles.searchViewContainer}
+        textInputStyle={styles.searchViewInput}
+        searchButtonStyle={styles.searchViewButton}
+        onSearchSubmit={props.onSearchSubmit}
+      />
     </View>
   );
 };
--- components/SearchView.js
+++ components/SearchView.js
@@ -1,12 +1,40 @@
 import React from "react";
-import { TextInput, Keyboard, View } from "react-native";
+import { TextInput, Keyboard, View, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
-import styles from "../styles/styles";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+
+const defaultStyles = StyleSheet.create({
+  searchViewContainer: {
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "space-evenly",
+  },
+  searchViewInput: {
+    borderWidth: 1,
+    width: "70%",
+    borderColor: "black",
+    alignSelf: "stretch",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 25,
+  },
+  searchViewButton: {
+    borderRadius: 25,
+    backgroundColor: "#00f",
+  },
+});
 
 export default class SearchView extends React.Component {
   state = {
     searchText: "",
+  };
+
+  static defaultProps = {
+    searchPrompt: "Search",
+    searchButtonPrompt: "",
+    containerStyle: defaultStyles.searchViewContainer,
+    textInputStyle: defaultStyles.searchViewInput,
+    searchButtonStyle: defaultStyles.searchViewButton,
   };
 
   onTextChange = (newText) => {
@@ -20,20 +48,39 @@ export default class SearchView extends React.Component {
 
   render() {
     return (
-      <View style={styles.searchViewContainer}>
+      <View
+        style={[
+          this.props.containerStyle,
+          {
+            flexDirection: "row",
+            alignContent: "center",
+            justifyContent: "space-evenly",
+          },
+        ]}
+      >
         <TextInput
-          style={styles.searchViewInput}
-          placeholder="Search Movie"
+          style={this.props.textInputStyle}
+          placeholder={this.props.searchPrompt}
           value={this.state.searchText}
           onChangeText={this.onTextChange}
         />
         <MaterialIcons.Button
           name="search"
+          style={() => {
+            let {
+              borderRadius,
+              backgroundColor,
+              ...rest
+            } = this.props.searchButtonStyle;
+            return rest;
+          }}
           onPress={this.onSearchSubmit}
-          backgroundColor="#00aaff"
-          borderRadius={50}
+          backgroundColor={this.props.searchButtonStyle.backgroundColor}
+          borderRadius={this.props.searchButtonStyle.borderRadius}
         >
-          Search
+          {this.props.searchButtonPrompt === ""
+            ? this.props.searchPrompt
+            : this.props.searchButtonPrompt}
         </MaterialIcons.Button>
       </View>
     );
@@ -42,4 +89,9 @@ export default class SearchView extends React.Component {
 
 SearchView.propTypes = {
   onSearchSubmit: PropTypes.func.isRequired,
+  searchPrompt: PropTypes.string,
+  searchButtonPrompt: PropTypes.string,
+  searchButtonStyle: PropTypes.object,
+  containerStyle: PropTypes.object,
+  textInputStyle: PropTypes.object,
 };
