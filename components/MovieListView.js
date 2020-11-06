cd7fb780adbf3c80a0fe6983ab0b98127dd6ec3e
the-it-weirdo <ddsmegh4@gmail.com>
2020-11-06 19:03:35 +0530

    Changes SectionList to FlatList

--- components/MovieListView.js
+++ components/MovieListView.js
@@ -1,37 +1,30 @@
 import React from "react";
-import { SectionList, Text } from "react-native";
+import { SectionList, Text, FlatList } from "react-native";
 import PropTypes from "prop-types";
 
 import RowItem from "./RowItem";
 
-const renderItem = ({ section, item }) => (
-  <RowItem movie={item} onMovieItemPress={section.onMovieItemPress} />
-);
+const renderItem = ({ item }) => {
+  console.log(item);
+  return <RowItem movie={item} onMovieItemPress={item.onMovieItemPress} />;
+};
 
 const MovieListView = (props) => {
   const list = props.movies;
 
-  const moviesByYear = list.reduce((obj, movie) => {
-    const year = movie.Year;
+  const data = list.map((obj) => {
     return {
       ...obj,
-      [year]: [...(obj[year] || []), movie],
+      onMovieItemPress: props.onMovieItemPress,
     };
-  }, {});
-
-  const onMovieItemPress = props.onMovieItemPress;
-
-  const sections = Object.keys(moviesByYear).map((year) => ({
-    data: moviesByYear[year],
-    title: year,
-    onMovieItemPress,
-  }));
+  });
 
   return (
-    <SectionList
-      sections={sections}
-      keyExtractor={(item, index) => item + index}
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.key}
       renderItem={renderItem}
+      ListEmptyComponent={() => <Text>Example</Text>}
     />
   );
 };
