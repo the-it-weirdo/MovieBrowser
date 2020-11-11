f02d82f2168d9e1e5def96cb0de851b7b6b7988f
the-it-weirdo <ddsmegh4@gmail.com>
2020-11-11 18:35:43 +0530

    App About work started

--- screens/AppAboutScreen.js
+++ screens/AppAboutScreen.js
@@ -1,11 +1,33 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View, SafeAreaView, ScrollView, StyleSheet, Text } from "react-native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+
+const styles = StyleSheet.create({
+  marginMedium: {
+    margin: 10,
+  },
+  aboutTitle: {
+    fontWeight: "bold",
+    fontSize: 24,
+    textAlign: "center",
+  },
+  aboutText: {
+    fontFamily: "monospace",
+    fontSize: 18,
+  }
+});
 
 const AppAboutScreen = () => {
   return (
-    <View>
-      <Text>App About</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <Text style={[styles.aboutTitle, styles.marginMedium]}>About</Text>
+        <Text style={[styles.marginMedium, styles.aboutText]}>
+          Movie Browser is an app to browse movies and lookup their ratings,
+          cast, director and brief plot details.
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
